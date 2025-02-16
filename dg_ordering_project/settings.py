3b6cf5514b572f3522c4276d2f5add6b880f3e64
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-02-16 11:50:53 +0000

    Update secret key value

--- dg_ordering_project/settings.py
+++ dg_ordering_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0xjl(by23a=(hr6z9@0ptj7qz+mrdk8h9vp(gif2mk9+%=jeiy'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

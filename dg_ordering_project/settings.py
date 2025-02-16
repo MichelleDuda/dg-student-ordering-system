627538624cfec4dd7bfaf1fed53a37d86c72a36f
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-02-16 11:23:44 +0000

    Set debug to True for troubleshooting

--- dg_ordering_project/settings.py
+++ dg_ordering_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0xjl(by23a=(hr6z9@0ptj7qz+mrdk8h9vp(gif2mk9+%=jeiy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 

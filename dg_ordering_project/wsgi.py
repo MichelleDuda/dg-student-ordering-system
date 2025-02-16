9d3ae4627b961eb44a13fb87057a2fcf9f59c9d1
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-02-16 11:27:44 +0000

    Fix spelling error in wsgi.py file

--- dg_ordering_project/wsgi.py
+++ dg_ordering_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dj_ordering_project project.
+WSGI config for dg_ordering_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_ordering_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dg_ordering_project.settings')
 
 application = get_wsgi_application()

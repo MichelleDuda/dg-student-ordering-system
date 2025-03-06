5a7a11602cf1aca00ef56fa44dc6062723e9609e
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-03-06 13:06:59 +0000

    Add content to sample_menu.html and ensure accessibility without login. Issue #8.

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from datetime import date
+from order.models import MenuWeek, Meal, Order, OrderItem
 # Views
 
 def about(request):

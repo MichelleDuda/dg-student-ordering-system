df4271817349e2df63dade9f96835aba80b03458
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-03-11 09:33:51 +0000

    Clean code for deployment

--- menu/forms.py
+++ menu/forms.py
@@ -29,14 +29,14 @@ class CustomUserChangeForm(UserChangeForm):
             self.add_error('confirm_password', "Passwords do not match.")
 
         return cleaned_data
-    
+
     def save(self, commit=True):
-        user=super().save(commit=False)
+        user = super().save(commit=False)
         password = self.cleaned_data.get("password")
 
         if password:
             user.set_password(password)
-        
+
         if commit:
             user.save()
         return user

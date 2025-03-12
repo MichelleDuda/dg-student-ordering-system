08733ce0a84635d578ae2cc6e1417692569db9fa
MichelleDuda <michelle.ann.hannon@gmail.com>
2025-03-12 08:49:12 +0000

    Add filter to Django admin panel for Meals

--- order/admin.py
+++ order/admin.py
@@ -5,6 +5,19 @@ from .models import MenuWeek, MealType, Meal, Order, OrderItem
 
 admin.site.register(MenuWeek)
 admin.site.register(MealType)
-admin.site.register(Meal)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = (
+        "menu_week",
+        "day_of_week",
+        "category",
+        "name",
+        "is_plant_based"
+    )
+    list_filter = ("menu_week", "category", "day_of_week")
+
+
+admin.site.register(Meal, MealAdmin)
